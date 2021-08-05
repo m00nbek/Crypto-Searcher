1e575e8094e7671f8e0815253fecb1f17f2f0766
m00nbek <melikulov990@gmail.com>
2021-08-05 22:34:55 +0500

    basic tableView setup in Home

--- Crypto Searcher/Modules/Home/View/HomeViewController.swift
+++ Crypto Searcher/Modules/Home/View/HomeViewController.swift
@@ -12,13 +12,51 @@ class HomeViewController: UIViewController, HomeViewProtocol {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavbar()
         configureUI()
     }
     // MARK: - Properties
     var presenter: HomePresenterProtcol?
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.coinCellIdentifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     // MARK: - Functions
     private func configureUI() {
         view.backgroundColor = .white
+        // constraints
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    private func configureNavbar() {
+        let search = UISearchController(searchResultsController: nil)
+        search.searchBar.delegate = self
+        navigationItem.searchController = search
     }
 }
-
+// MARK: - UISearchBarDelegate
+extension HomeViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // search
+    }
+}
+// MARK: - UITableViewDelegate/DataSource
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.coinCellIdentifier, for: indexPath)
+        return cell
+    }
+    
+}
---
+++ Crypto Searcher/Supporting files/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Crypto Searcher
+//
+//  Created by Oybek on 8/5/21.
+//
+
+import Foundation
+
+class Constants {
+    static let coinCellIdentifier = "coinCell"
+}
